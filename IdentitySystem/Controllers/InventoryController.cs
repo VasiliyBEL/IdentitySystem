32b75e65bb5468681dccad46eef8354f14224e20
VasiliyBEL <vaspn001@gmail.com>
2023-05-17 13:29:34 +0300

    Add project files.

---
+++ IdentitySystem/Controllers/InventoryController.cs
@@ -0,0 +1,15 @@
+﻿using IdentitySystem.Constants;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentitySystem.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class InventoryController : Controller
+    {
+        public IActionResult GetAll()
+        {
+            return View();
+        }
+    }
+}
